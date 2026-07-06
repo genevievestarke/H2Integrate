8ce9457a0dc0d1c78c704e863e6099171c668a5a
Genevieve Starke <genevieve.starke@nrel.gov>
2026-07-06 17:39:12 -0400

    Add diesel model for FC datacenter work

---
+++ h2integrate/converters/diesel/__init__.py
@@ -0,0 +1,14 @@
+from h2integrate.converters.natural_gas.natural_gas_cc_ct import (
+    NaturalGasPerformanceModel,
+    NaturalGasCostModel,
+)
+from h2integrate.converters.natural_gas.dummy_gas_components import (
+    SimpleGasProducerPerformance,
+    SimpleGasProducerCost,
+    SimpleGasConsumerPerformance,
+    SimpleGasConsumerCost,
+)
+from h2integrate.converters.natural_gas.SO_NG_fuel_cell import (
+    SONGFuelCellPerformanceModel,
+    SONGFuelCellCostModel,
+)
